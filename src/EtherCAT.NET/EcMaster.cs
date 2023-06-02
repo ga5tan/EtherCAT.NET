143e42f7c17676d68d8ab505634090cca62042fc
Juraj Benadik <ga5tan@outlook.com>
2023-06-02 10:43:09 +0200

    Do not reset sync managers if ESI op mode -> sm does not list any PDOs

--- src/EtherCAT.NET/EcMaster.cs
+++ src/EtherCAT.NET/EcMaster.cs
@@ -160,17 +160,12 @@ namespace EtherCAT.NET
 
             foreach (DataDirection dataDirection in Enum.GetValues(typeof(DataDirection)))
             {
-                switch (dataDirection)
+                slavePdoOffsets = dataDirection switch
                 {
-                    case DataDirection.Output:
-                        slavePdoOffsets = slaveRxPdoOffsets;
-                        break;
-                    case DataDirection.Input:
-                        slavePdoOffsets = slaveTxPdoOffsets;
-                        break;
-                    default:
-                        throw new NotImplementedException();
-                }
+                    DataDirection.Output => slaveRxPdoOffsets,
+                    DataDirection.Input => slaveTxPdoOffsets,
+                    _ => throw new NotImplementedException(),
+                };
 
                 foreach (var slave in slaves)
                 {
@@ -181,21 +176,11 @@ namespace EtherCAT.NET
                         ioMapBitOffset = 0;
 
                     ioMapByteOffset = slaveByteOffset;
-
-                    Console.WriteLine($"ConfigureIoMap - before loop, slave.DynamicData.Pdos: {slave.DynamicData.Pdos.Count}");
-                    var matchingVariables = slave.DynamicData.Pdos
-                    .Where(x => x.SyncManager >= 0)
-                    .SelectMany(x => x.Variables)
-                    .Where(x => x.DataDirection == dataDirection)
-                    .ToList();
-
-                    foreach (var variable in matchingVariables)
-                    {
-                        Console.WriteLine("ConfigureIoMap - Variable found: " + variable.Name);
-                        // Your logic for processing the variable goes here
-                    }
-                    if (matchingVariables.Count < 1) Console.WriteLine("ConfigureIoMap - No variables match the criteria on the line 185");
-                    foreach (var variable in slave.DynamicData.Pdos.Where(x => x.SyncManager >= 0).ToList().SelectMany(x => x.Variables).ToList().Where(x => x.DataDirection == dataDirection))
+                    
+                    foreach (var variable in slave.DynamicData.Pdos
+                        .Where(pdo => pdo.SyncManager >= 0)
+                        .SelectMany(pdo => pdo.Variables)
+                        .Where(variable => variable.DataDirection == dataDirection))
                     {
                         variable.DataPtr = IntPtr.Add(_ioMapPtr, ioMapByteOffset);
                         variable.BitOffset = ioMapBitOffset;
